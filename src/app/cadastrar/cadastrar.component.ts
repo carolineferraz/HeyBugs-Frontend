9bc16504402b5dc9ade6d8d266b6317353d20987
Caroline Ferraz <projetos.crrz@gmail.com>
2022-06-01 23:11:38 -0300

    Feat: implementando entrar.component.ts (ainda faltando ajustes)

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../Model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = new Usuario
+  confirmarSenha: string // o nome da variável foi colocado como confirmarSenha, com "r", pois o nome da variável não pode ser igual ao nome do método "confirmaSenha()"
+  tipoDeUsuario: string
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
+
   }
+
+  confirmaSenha(event: any) {
+    this.confirmarSenha = event.target.value
+  }
+
+  tipoUsuario(event: any) {
+    this.tipoDeUsuario = event.target.value
+  }
+
+  cadastrar() {
+    this.usuario.tipo = this.tipoDeUsuario
+
+    if(this.usuario.senha != this.confirmarSenha){
+      alert('As senhas não coincidem.')
+    } else{
+        this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {
+          this.usuario = resp
+          this.router.navigate(['/entrar'])
+          alert('Usuário cadastrado com sucesso')
+        })
+    }
+  }
+
+
 
 }
