33241576a81b407dd3c4ae4613a001c793be3a02
Caroline Ferraz <projetos.crrz@gmail.com>
2022-06-08 19:17:06 -0300

    Feat: implementando o ngFor para listagem de temas no option do modal do componente inicio

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../Model/Postagem';
+import { Tema } from '../Model/Tema';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,8 +13,13 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
+  postagem: Postagem = new Postagem()
+  listaTemas: Tema[]
+
   constructor(
-    private router: Router
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService
   ) { }
 
   ngOnInit() {
@@ -19,6 +28,19 @@ export class InicioComponent implements OnInit {
       alert('Sua sessão expirou! Faça login novamente.')
       this.router.navigate(['/entrar'])
     }
+
+    this.getAllTemas()
+
+  }
+
+  getAllTemas() {
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+  publicar() {
+
   }
 
 }
